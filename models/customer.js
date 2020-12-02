b1f5fd29b5d5cc67144383816ad1342393b3d5a8
Brian D. Shapiro <briand.shapiro@Brians-MBP.attlocal.net>
2020-12-02 13:35:12 -0500

    Part 5: Full Names

--- models/customer.js
+++ models/customer.js
@@ -6,78 +6,91 @@ const Reservation = require("./reservation");
 /** Customer of the restaurant. */
 
 class Customer {
-  constructor({ id, firstName, lastName, phone, notes }) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phone = phone;
-    this.notes = notes;
-  }
+	constructor({ id, firstName, lastName, phone, notes }) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.notes = notes;
+	}
 
-  /** find all customers. */
+	/** find all customers. */
 
-  static async all() {
-    const results = await db.query(
-      `SELECT id, 
+	static async all() {
+		const results = await db.query(
+			`SELECT id, 
          first_name AS "firstName",  
          last_name AS "lastName", 
          phone, 
          notes
        FROM customers
        ORDER BY last_name, first_name`
-    );
-    return results.rows.map(c => new Customer(c));
-  }
+		);
+		return results.rows.map((c) => new Customer(c));
+	}
 
-  /** get a customer by ID. */
+	/** get a customer by ID. */
 
-  static async get(id) {
-    const results = await db.query(
-      `SELECT id, 
+	static async get(id) {
+		const results = await db.query(
+			`SELECT id, 
          first_name AS "firstName",  
          last_name AS "lastName", 
          phone, 
          notes 
         FROM customers WHERE id = $1`,
-      [id]
-    );
+			[id]
+		);
 
-    const customer = results.rows[0];
+		const customer = results.rows[0];
 
-    if (customer === undefined) {
-      const err = new Error(`No such customer: ${id}`);
-      err.status = 404;
-      throw err;
-    }
+		if (customer === undefined) {
+			const err = new Error(`No such customer: ${id}`);
+			err.status = 404;
+			throw err;
+		}
 
-    return new Customer(customer);
-  }
+		return new Customer(customer);
+	}
 
-  /** get all reservations for this customer. */
+	/** get full name of customer instance */
+	async fullName() {
+		const results = await db.query(
+			`SELECT first_name, last_name
+      FROM customers
+      WHERE id=$1
+      RETURNING first_name, last_name`,
+			[this.id]
+		);
+		const customer = results.rows[0];
+		return [customer.first_name, customer.last_name].join(" ");
+	}
 
-  async getReservations() {
-    return await Reservation.getReservationsForCustomer(this.id);
-  }
+	/** get all reservations for this customer. */
 
-  /** save this customer. */
+	async getReservations() {
+		return await Reservation.getReservationsForCustomer(this.id);
+	}
 
-  async save() {
-    if (this.id === undefined) {
-      const result = await db.query(
-        `INSERT INTO customers (first_name, last_name, phone, notes)
+	/** save this customer. */
+
+	async save() {
+		if (this.id === undefined) {
+			const result = await db.query(
+				`INSERT INTO customers (first_name, last_name, phone, notes)
              VALUES ($1, $2, $3, $4)
              RETURNING id`,
-        [this.firstName, this.lastName, this.phone, this.notes]
-      );
-      this.id = result.rows[0].id;
-    } else {
-      await db.query(
-        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
+				[this.firstName, this.lastName, this.phone, this.notes]
+			);
+			this.id = result.rows[0].id;
+		} else {
+			await db.query(
+				`UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
              WHERE id=$5`,
-        [this.firstName, this.lastName, this.phone, this.notes, this.id]
-      );
-    }
-  }
+				[this.firstName, this.lastName, this.phone, this.notes, this.id]
+			);
+		}
+	}
 }
 
 module.exports = Customer;
